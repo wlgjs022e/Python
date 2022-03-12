6872e7a99e66cca89f2351a3ef33c763820dc2d1
jiheon kim <wlgjs0223@naver.com>
2022-03-13 03:00:30 +0900

    숫자 더하기 함수

--- python/11720.py
+++ python/11720.py
@@ -1,14 +1,10 @@
 def solve():
 
-
     a = int(input())
-
+    cnt = 0
+    num_list = list(map(int, input()))
     for i in range(len(num_list)):
         cnt += num_list[i]
-    cnt = 0
-    num_list = list(map(int,input()))
 
     print(cnt)
-
-
 solve()
