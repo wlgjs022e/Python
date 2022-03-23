9cd40cef5498b0059abe46ca07af281489f136a7
wlgjs022e <wlgjs0223@gmail.com>
2022-03-24 03:35:31 +0900

    덩치

---
+++ python/7568.py
@@ -0,0 +1,26 @@
+
+def solve():
+
+    a = int(input())
+    cnt = 0
+    temp = []
+    answer = []
+
+
+    for i in range(a):
+        c = list(map(int,input().split()))
+        temp.append(c)
+
+
+    for i in range(a):
+        cnt = 1
+        for j in range(a):
+            if temp[i][0] < temp[j][0] and temp[i][1] < temp[j][1]:
+                cnt += 1
+        answer.append(cnt)
+    for i in answer:
+        print(i,end=" ")
+
+
+
+solve()
