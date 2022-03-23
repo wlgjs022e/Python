81206dc4f8d832a58d379d1af43848a00876909f
wlgjs022e <wlgjs0223@gmail.com>
2022-03-23 23:18:00 +0900

    생성자 찾기

---
+++ python/10870.py
@@ -0,0 +1,29 @@
+def solve():
+
+    a = int(input())
+    temp = a
+    cnt = 1
+    num1 = 0
+    num2 = 1
+    while(True):
+
+        if a == 0:
+            print(0)
+            break
+        elif a == 1:
+            print(1)
+            break
+
+        num1 += num2
+        cnt += 1
+        if (cnt == temp and cnt % 2 == 0):
+            print(num1)
+            break
+
+        num2 += num1
+        cnt += 1
+        if (cnt == temp and cnt % 2 != 0):
+            print(num2)
+            break
+
+solve()
